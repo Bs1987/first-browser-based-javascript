ea31674dab62f0a28273be0d0004df228062d2d7
Bs1987 <boysax@gmail.com>
2021-05-01 14:03:04 +0200

    clean up!

--- main.js
+++ main.js
@@ -2,13 +2,9 @@
 // scripts om de waarschuwing in te voeren
 
 const usernameInput = document.getElementById("input-username");
-console.log(usernameInput);
-
 const usernameWarning = document.getElementById("username-warning");
-console.log(usernameWarning);
 
 usernameInput.addEventListener('keyup', checkUsername);
-
 function checkUsername(e) {
     // if usernameInput contains "@", show following message
     if (e.target.value.includes('@')) {
