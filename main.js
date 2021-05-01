e64a406cb9e6df063ceea210ef1ae5d9b87edca6
Bs1987 <boysax@gmail.com>
2021-05-01 14:15:06 +0200

    made function

--- main.js
+++ main.js
@@ -23,3 +23,14 @@ const passwordInput = document.getElementById("input-password");
 const passwordWarning = document.getElementById("password-warning");
 
 passwordInput.addEventListener('keyup', checkPassword);
+
+function checkPassword(e) {
+    // if passwordInput =< 6, or passwordInput is empty, give warning message. When passwordInput > 6, give no warning
+    if (e.target.value === "" || e.target.value.length > 6) {
+        console.log("safe")
+        passwordWarning.textContent = " "
+    }else {
+        console.log("unsafe")
+        passwordWarning.textContent = "Wachtwoord is te kort, gebruik minimaal 6 tekens!"
+    }
+}
