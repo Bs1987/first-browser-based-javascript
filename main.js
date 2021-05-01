59679e053a382b319650b774d1c657e43c5dd70c
Bs1987 <boysax@gmail.com>
2021-05-01 14:04:46 +0200

    made plan for checking passwords

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-// scripts om te checken voor 6 of meer cijfers
-// scripts om de waarschuwing in te voeren
-
 const usernameInput = document.getElementById("input-username");
 const usernameWarning = document.getElementById("username-warning");
 
 usernameInput.addEventListener('keyup', checkUsername);
+
 function checkUsername(e) {
     // if usernameInput contains "@", show following message
     if (e.target.value.includes('@')) {
@@ -14,3 +12,9 @@ function checkUsername(e) {
         usernameWarning.textContent = '';
     }
 }
+
+// steps for checking password:
+//
+// [] - make variables
+// [] - make eventlistener
+// [] - make function
