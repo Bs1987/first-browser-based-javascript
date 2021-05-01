49a3874bb391bf6db6ef5bbbbd85b1726528d1c6
Bs1987 <boysax@gmail.com>
2021-05-01 13:41:25 +0200

    made plan for username warning

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 // scripts om the checken voor @
 // scripts om te checken voor 6 of meer cijfers
 // scripts om de waarschuwinging in te voeren
+
+//plan for username warning:
+
+//if we find an "@"
+//display warning message
+//if there is no "@"
+//don't display warning message
+
+
+
