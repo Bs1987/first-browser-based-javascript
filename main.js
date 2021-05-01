1d8853036ad53da832bac3e2c5808f1b6b56b169
Bs1987 <boysax@gmail.com>
2021-05-01 14:02:18 +0200

    clean up!

--- main.js
+++ main.js
@@ -1,21 +1,9 @@
-// scripts om the checken voor @
 // scripts om te checken voor 6 of meer cijfers
 // scripts om de waarschuwing in te voeren
 
-//plan for username warning:
-
-//if we find an "@"
-//display warning message
-//if there is no "@"
-//don't display warning message
-
-// Steps:
-
-// [X] - make variable to check username form
 const usernameInput = document.getElementById("input-username");
 console.log(usernameInput);
 
-// [X] - make variable to input string into username warning
 const usernameWarning = document.getElementById("username-warning");
 console.log(usernameWarning);
 
@@ -30,4 +18,3 @@ function checkUsername(e) {
         usernameWarning.textContent = '';
     }
 }
-// [X] - make function to act on output from username check
